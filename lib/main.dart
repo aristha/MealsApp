3fc78df1ac5357a0f6f01de403125af5c7de90c7
tam <tam@gmail.com>
2023-10-07 02:08:55 +0900

    update day one leanr app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/categories.dart';
+import 'package:mealsapp/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
---
+++ lib/screens/categories.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:mealsapp/data/dummy_data.dart';
+import 'package:mealsapp/models/category.dart';
+import 'package:mealsapp/screens/meals.dart';
+import 'package:mealsapp/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) =>
+            MealsScreen(title: category.title, meals: filteredMeals)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pick your category'),
+      ),
+      body: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: [
+            for (final category in availableCategories)
+              CategoryGridItem(
+                category: category,
+                onSelectCategory: () {
+                  _selectCategory(context, category);
+                },
+              )
+          ]),
+    );
+  }
+}
